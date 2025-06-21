1d781c071a373f195bcd81864d46ad4ef23ee80c
RehIreh <kadek.heriawan511@gmail.com>
2025-06-21 10:05:32 +0800

    Miiaww

--- config/firebase.jsx
+++ config/firebase.jsx
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-import { firebaseConfig } from "./firebaseConfig"; // mengambil config dari file terpisah
+import { firebaseConfig } from "./firebaseConfig"; 
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
